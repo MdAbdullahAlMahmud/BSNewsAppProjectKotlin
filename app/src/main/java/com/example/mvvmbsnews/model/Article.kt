c53901a83c619e4e9a982ea5017fe9d403a8f2d9
Abdullah Al Mahmud <mmunna864@gmail.com>
2023-04-04 10:44:50 +0600

    Add serializable declaration on Article class

--- app/src/main/java/com/example/mvvmbsnews/model/Article.kt
+++ app/src/main/java/com/example/mvvmbsnews/model/Article.kt
@@ -2,6 +2,7 @@ package com.example.mvvmbsnews.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.io.Serializable
 
 @Entity(tableName = "articles")
 data class Article(
@@ -16,4 +17,4 @@ data class Article(
     val title: String,
     val url: String,
     val urlToImage: String
-)
+) : Serializable
